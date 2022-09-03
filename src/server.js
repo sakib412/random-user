c3c7911fb6e0ce28f67a8084c2e1bb345d2259c7
Najmus Sakib <nazmusakib412@gmail.com>
2022-09-03 22:46:02 +0600

    Add: server js

---
+++ src/server.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import { json, urlencoded } from 'body-parser';
+import 'dotenv/config'
+
+import config from './config';
+import errorHandler from './middleware/errorHandler';
+import { successResponse } from './utils/response';
+
+export const app = express();
+
+// Middleware
+app.disable('x-powered-by')
+app.use(cors())
+app.use(json())
+app.use(urlencoded({ extended: true }))
+app.use(morgan('dev'))
+
+// Routes
+app.get('/', (req, res) => {
+    res.json(successResponse({ "message": "Server is running" }));
+})
+
+
+// handle errors
+app.use(errorHandler)
+
+export const start = async () => {
+    try {
+        app.listen(config.port, () => {
+            console.log(`App listening on PORT: ${config.port}`)
+        })
+    } catch (e) {
+        console.error(e)
+    }
+}
